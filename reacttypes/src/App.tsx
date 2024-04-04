36ddd8292883eee420c0210eda4751f99898b1cf
etimbuk-bassey <ehrtyc@gmail.com>
2024-04-04 15:43:30 +0100

    new update

--- reacttypes/src/App.tsx
+++ reacttypes/src/App.tsx
@@ -1,19 +1,25 @@
 import "./App.css";
+// import Users from "./components/Users";
 import Todo from "./components/Todo";
 
 function App() {
+  const options = ["brown", "black", "blue"];
 
-  const options = ["brown", "black", "blue"]
+  const [color1, , color3] = options;
 
-  const [color1, color2, color3] = options
+  console.log(`${color1} ${color3}`);
 
-  console.log(color1);
-  console.log(color2);
-  console.log(color3);
-  
-  
-  
+  const color = {
+    color4: "green",
+    color5: "pink",
+    color7: "white",
+  };
 
+  const { color4: C, color7 } = color;
+
+  console.log(`${C} ${color7}`);
+
+  // const
 
   return (
     <>
